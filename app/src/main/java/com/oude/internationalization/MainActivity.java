82e7ae1815a8fcec3b3afedf52cd9179c6680a85
Karthrand <735867580@qq.com>
2018-06-28 17:44:29 +0800

    完成基本界面

--- app/src/main/java/com/oude/internationalization/MainActivity.java
+++ app/src/main/java/com/oude/internationalization/MainActivity.java
@@ -2,13 +2,56 @@ package com.oude.internationalization;
 
 import android.app.*;
 import android.os.*;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
+import android.support.v4.view.GravityCompat;
+import android.support.design.widget.NavigationView;
 
-public class MainActivity extends Activity 
+public class MainActivity extends AppCompatActivity
 {
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.mainDrawerLayout1);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        navView.setCheckedItem(R.id.nav_language);
+        navView.setItemIconTintList(null);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+            });
+        
+        
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+        }
+        return true;
+    }
+    
+    
+    
 }
